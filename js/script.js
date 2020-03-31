69a9ae194297eba9c2c9164d0a7989fe82fad7e2
lyamtorres <lyamtorres@hotmail.com>
2020-03-31 17:39:30 +0200

    Contact form finished

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+var prevScrollPos = window.pageYOffset;
+window.onscroll = () => {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollPos > currentScrollPos) {
+        document.getElementById("navbar").style.top = 0;
+    } else {
+        document.getElementById("navbar").style.top = "-98px";
+    }
+    prevScrollPos = currentScrollPos;
+};
+*/
