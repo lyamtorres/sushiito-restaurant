c7a2eeccaa2209a118f211d87c20bb63ccd5496c
lyamtorres <lyamtorres@hotmail.com>
2020-04-01 23:13:31 +0200

    Reservation button compatibility and layout adjusted

--- js/script.js
+++ js/script.js
@@ -1,12 +1,56 @@
-/*
-var prevScrollPos = window.pageYOffset;
-window.onscroll = () => {
-    var currentScrollPos = window.pageYOffset;
-    if (prevScrollPos > currentScrollPos) {
-        document.getElementById("navbar").style.top = 0;
-    } else {
-        document.getElementById("navbar").style.top = "-98px";
-    }
-    prevScrollPos = currentScrollPos;
+/* Activates smooth scroll for navigation in the Events section */
+
+var smoothScroll = (target, duration) => {
+    var target = document.querySelector(target);
+    var targetPosition = target.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition;
+    var startTime = null; 
+
+    var animation = (currentTime) => {
+        if(startTime === null) startTime = currentTime;
+            var timeElapsed = currentTime - startTime;
+            var run = ease(timeElapsed, startPosition, distance, duration);
+            window.scrollTo(0, run);
+            if(timeElapsed < duration) requestAnimationFrame(animation);
+            };
+
+    var ease = (t, b, c, d) => {
+    t /= d/2;
+	if (t < 1) return c/2*t*t*t*t + b;
+	t -= 2;
+	return -c/2 * (t*t*t*t - 2) + b;
+    };
+
+    requestAnimationFrame(animation);
 };
-*/
+
+const eventsLinkOne = document.querySelector('.events-link-one');
+eventsLinkOne.addEventListener('click', () => {
+    smoothScroll('.main-events', 1000);
+});
+
+const eventsLinkTwo = document.querySelector('.events-link-two');
+eventsLinkTwo.addEventListener('click', () => {
+    smoothScroll('.main-events', 1000);
+});
+
+const birthdayLinkOne = document.querySelector('.birthday-link-one');
+birthdayLinkOne.addEventListener('click', () => {
+    smoothScroll('.birthday', 1000);
+});
+
+const birthdayLinkTwo = document.querySelector('.birthday-link-two');
+birthdayLinkTwo.addEventListener('click', () => {
+    smoothScroll('.birthday', 1000);
+});
+
+const poolLinkOne = document.querySelector('.pool-link-one');
+poolLinkOne.addEventListener('click', () => {
+    smoothScroll('.pool', 1000);
+});
+
+const poolLinkTwo = document.querySelector('.pool-link-two');
+poolLinkTwo.addEventListener('click', () => {
+    smoothScroll('.pool', 1000);
+});
